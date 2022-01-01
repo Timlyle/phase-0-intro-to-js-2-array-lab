aa541b35a90175d5dd8c11512001a8f720a67e69
Timothy Lyle <itsfreezingouthere@yahoo.com>
2022-01-01 13:00:33 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(catname) {
+    cats.push(catname);
+};
+
+function destructivelyPrependCat(catname){
+    cats.unshift(catname);
+
+};
+
+function destructivelyRemoveLastCat(catname) {
+    cats.pop(catname);
+};
+
+function destructivelyRemoveFirstCat(catname) {
+     cats.shift(catname);
+};
+
+function appendCat(catname) {
+    return [...cats, catname];
+    
+};
+
+function prependCat(catname) {
+    return [catname,...cats]
+};
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+}; 
+
+function removeFirstCat() {
+ return cats.slice(1)
+};
